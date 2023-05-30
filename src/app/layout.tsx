ee56139bbdaea2211766bdc9a79a9d2291026d81
Mark Vujevits <vujevits.mark@gmail.com>
2023-05-30 16:21:26 +0200

    change to header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <div/>
+      <header/>
       <body className={inter.className}>{children}</body>
     </html>
   )
